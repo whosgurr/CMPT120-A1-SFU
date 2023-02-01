948b50d008d422fde128668e37a83fee30b83ff2
Yuen Wai Mo <garymo@hotmail.ca>
2023-01-31 16:23:52 -0800

    Q2 Submission Ready

--- q2.py
+++ q2.py
@@ -26,24 +26,21 @@ def main():
         if operator_select in [1, 2, 3, 4]:
             x = int(input("Enter the first number in the interval of [0,100]:\n"))
             y = int(input("Enter the second number in the interval of [0,100]:\n"))
+            if -1 < x < 101 and -1 < y < 101:
+                if operator_select == 1:
+                    print(f"{x} + {y} = {addition_function(x, y)}")
+                elif operator_select == 2:
+                    print(f"{x} - {y} = {subtraction_function(x, y)}")
+                elif operator_select == 3:
+                    print(f"{x} * {y} = {multiply_function(x, y)}")
+                elif operator_select == 4 and y != 0:
+                    print(f"{x} / {y} = {division_function(x, y)}")
+                    if y == 0:
+                        print("The denominator cannot be 0.")
+            else:
+                print("Magic calculator can not perform your operation!")
         else:
             print("Invalid input!")
-            exit()
-
-        if -1 < x < 101 and -1 < y < 101:
-            if operator_select == 1:
-                print(f"{x} + {y} = {addition_function(x, y)}")
-            elif operator_select == 2:
-                print(f"{x} - {y} = {subtraction_function(x, y)}")
-            elif operator_select == 3:
-                print(f"{x} * {y} = {multiply_function(x, y)}")
-            elif operator_select == 4 and y != 0:
-                print(f"{x} / {y} = {division_function(x, y)}")
-                if y == 0:
-                    print("The denominator cannot be 0.")
-
-        else:
-            print("Magic calculator can not perform your operation!")
 
 
 if __name__ == '__main__':
