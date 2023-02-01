bb547c3f21a372d8f3b780f70c6241531264ab53
Yuen Wai Mo <garymo@hotmail.ca>
2023-02-01 00:04:38 -0800

    Complete Q2 or Q3

--- q3.py
+++ q3.py
@@ -1,27 +1,32 @@
+
 def is_not_multiple(number):
     for i in range(2, number):
         if number % i == 0:
-            return True
-    return False
+            return False
+    return True
 
 
 def user_prompt():
-
+    i = 0
     num_primes = 0
+
     iterations = int(input("How many numbers do you want to check?\n"))
 
-    for i in range(iterations):
+    while i != iterations:
         n = int(input("Enter a positive integer:\n"))
-        if n % 2 == 0 and n != 2:
+
+        if n < 0:
+            print("PrimeFinder ignores negative numbers!")
+            i -= 1
+
+        elif (n % 2 == 0 and n != 2) or n == 1:
+            i += 1
             continue
 
-        elif n < 0:
-            print("PrimeFinder ignores negative numbers!")
-            iterations += 1
-
-        else:
+        elif is_not_multiple(n):
             print(f"{n} is a prime number.")
             num_primes += 1
+        i += 1
 
     print(f"Total prime numbers: {num_primes}")
 
--- q4.py
+++ q4.py
@@ -1,9 +1,12 @@
 
 
+def PyNum():
+    for x in range(1, x+1):
+        for y in range(1, (rows-i)+1):
+            print(end=" ")
 
 
 def main():
-
 
 
 if __name__ == '__main__':
