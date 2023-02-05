50a1d584b5de66fc5477baa726c172c1e6362f99
Yuen Mo <garymo@hotmail.ca>
2023-02-04 22:11:10 -0800

    Q4 beginning logic

--- q4.py
+++ q4.py
@@ -1,12 +1,23 @@
+def valid_check(user_input):
+    if 2 < user_input < 9:
+        return True
 
+    else:
+        print("PyNum cannot help you!")
+        return False
 
-def PyNum():
-    for x in range(1, x+1):
-        for y in range(1, (rows-i)+1):
-            print(end=" ")
+def PyNum(h):
+    for rows in range(h):
+        for columns in range(h):
+            print(columns, end=" ")
+        print("\n")
 
 
 def main():
+    h = int(input("What is the height of the pyramid?\n"))
+    if valid_check(h):
+        PyNum(h)
+
 
 
 if __name__ == '__main__':
